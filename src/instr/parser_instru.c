53d56029dcf67eb9885f36a628c16f699091f49f
Jean <jean.begaint@gmail.com>
2013-11-03 13:31:15 +0100

    syntac

--- src/instr/parser_instru.c
+++ src/instr/parser_instru.c
@@ -1,47 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-#include "arch/arch.h"
-
-#include "instr/parser_instru.h"
-
-uint parser_instr(uint val, uchar start, uchar stop)
-{
-    int i;
-    uint isol = 0;
-    uint res;
-
-    for (i=start; i<stop+1; i++){
-        isol |= (1<<i) ;
-    }
-    res = val & isol;
-    res = res >> start;
-    
-    return res;
-}
-
-
-int parser_typeR(uint val , uint *rs, uint *rt, uint *rd, uint *sa)
-{
-    *rs = parser_instr(val, 21, 25 );
-    *rt = parser_instr(val, 16, 20 );
-    *rd = parser_instr(val, 11, 15 );
-    *sa = parser_instr(val, 6, 10 );
-
-    return 1;
-}
-
-int parser_typeI(uint val, uint *rs, uint *rt, uint *immediate)
-{
-    *rs = parser_instr(val, 21, 25 );
-    *rt = parser_instr(val, 16, 20 );
-    *immediate = parser_instr(val, 0, 15 );
-
-    return 1;
-}
-int parser_typeJ(uint val, uint *target)
-{
-    *target = parser_instr(val, 0, 25 );
-    return 1;
-}
+#include <stdio.h>#include <stdlib.h>#include <math.h>#include "arch/arch.h"#include "instr/parser_instru.h"uint parser_instr(uint val, uchar start, uchar stop){    int i;    uint isol = 0;    uint res;    for (i=start; i<stop+1; i++){        isol |= (1<<i) ;    }    res = val & isol;    res = res >> start;        return res;}int parser_typeR(uint val , uint *rs, uint *rt, uint *rd, uint *sa){    *rs = parser_instr(val, 21, 25 );    *rt = parser_instr(val, 16, 20 );    *rd = parser_instr(val, 11, 15 );    *sa = parser_instr(val, 6, 10 );    return 1;}int parser_typeI(uint val, uint *rs, uint *rt, uint *immediate){    *rs = parser_instr(val, 21, 25 );    *rt = parser_instr(val, 16, 20 );    *immediate = parser_instr(val, 0, 15 );    return 1;}int parser_typeJ(uint val, uint *target){    *target = parser_instr(val, 0, 25 );    return 1;}
--- src/utils.c
+++ src/utils.c
@@ -9,7 +9,7 @@
 #include "notify.h"
 #include "utils.h"
 
-void usage(char* arg)
+void usage(char *arg)
 {
 	printf("Usage: %s [file]\n", arg);
 }
@@ -20,10 +20,10 @@ void usage(char* arg)
 	@return FILE* or NULL if file does not exist
 */
 
-FILE* open_file(char* filepath)
+FILE *open_file(char *filepath)
 {
 	char buffer[256];
-	FILE* f = NULL;
+	FILE *f = NULL;
 
 	f = fopen(filepath, "r");
 
@@ -38,13 +38,13 @@ FILE* open_file(char* filepath)
 	return f;
 }
 
-void close_file(FILE* f)
+void close_file(FILE * f)
 {
 	fclose(f);
 	DEBUG_MSG("file closed");
 }
 
-int quit(ARCH arch) 
+int quit(ARCH arch)
 {
 	print_info("Exiting...");
 	free_arch(arch);
@@ -59,26 +59,27 @@ int die(ARCH arch)
 	exit(EXIT_FAILURE);
 }
 
-void print_str(char* str, char* type, int color)
+void print_str(char *str, char *type, int color)
 {
 
 	time_t timer;
-    char buffer[25];
-    struct tm* tm_info;
+	char buffer[25];
+	struct tm *tm_info;
 
-    time(&timer);
-    tm_info = localtime(&timer);
+	time(&timer);
+	tm_info = localtime(&timer);
 
-    strftime(buffer, 25, "%H:%M:%S", tm_info);
-	fprintf(stderr, "\x1b[%dm[ %s | %s: %s ]\n\x1b[0m", color, buffer, type, str);
+	strftime(buffer, 25, "%H:%M:%S", tm_info);
+	fprintf(stderr, "\x1b[%dm[ %s | %s: %s ]\n\x1b[0m", color, buffer,
+		type, str);
 }
 
-void print_info(char* str)
+void print_info(char *str)
 {
 	print_str(str, "Info", 34);
 }
 
-void print_error(char* str)
+void print_error(char *str)
 {
 	print_str(str, "Error", 31);
 }
@@ -91,25 +92,25 @@ void print_section_info(SECTION section)
 	}
 }
 
-uint get_hash(char* cmd)
+uint get_hash(char *cmd)
 {
 	uint i;
 	uint res = 0;
-	
-	for (i=0; i < strlen(cmd) && i < 2 ; i++) {
-		res = res*100;
-		res = res + (*(cmd+i) - 'a' + 1);
+
+	for (i = 0; i < strlen(cmd) && i < 2; i++) {
+		res = res * 100;
+		res = res + (*(cmd + i) - 'a' + 1);
 	}
 	return res;
 }
 
-char bin_to_dec(char* n)
+char bin_to_dec(char *n)
 {
 	uint i;
 	char res = 0;
-	
+
 	for (i = 0; i < strlen(n); i++) {
-		res = res*2 + (n[i] - (int)'0');
+		res = res * 2 + (n[i] - (int) '0');
 	}
 
 	return res;
